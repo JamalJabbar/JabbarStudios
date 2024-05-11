bdcacbd58de1c9c41e3e881abc68e4b81c99ff89
Jamal Jabbar <jamal.jabbar10@gmail.com>
2024-05-11 17:14:57 -0500

    Added progress slider to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence, useScroll, useSpring } from 'framer-motion';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
@@ -9,9 +9,18 @@ function App() {
     window.history.scrollRestoration = 'manual';
   }, [location]);
 
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
+
   return (
     <>
       <AnimatePresence>
+        <motion.div className="progress-bar" style={{ scaleX }} />
         <Routes key={location.pathname} location={location}>
           <Route path="/" element={<HomePage />}>
             {/* <Route path="page1" element={<Page1 />} /> */}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
-import * as React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Image, Text, Spacer, VStack } from '@chakra-ui/react';
+import { motion, useInView, } from 'framer-motion';
 
 import Subheader from '../components/Subheader';
 import Reveal from '../utils/Reveal';
 
 export default function Header() {
+  // const ref = useRef(null)
+  // const isInView = useInView(ref)
+
+  // useEffect(() => {
+  //   console.log(`The element is ${isInView ? 'in view' : 'NOT in view'}`)
+
+  // }, [isInView])
+
   return (
     <VStack
       position="absolute"
@@ -23,7 +32,7 @@ export default function Header() {
           pt={20}
           userSelect="none"
           width={{ base: '90vw', md: '60vw', xl: '50vw%', '2xl': '40vw' }}
-          src="/jabbar-studios.svg"
+          src="/jabbar.svg"
         ></Image>
       </Reveal>
       <Reveal>
