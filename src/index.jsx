1edcb6ef4ea3160157b1c5c6fd3f53626f511401
Jamal Jabbar <jamal.jabbar10@gmail.com>
2024-05-11 19:14:48 -0500

    Overrided chakra theme to change bg color

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import HomePage from './pages/HomePage';
 import App from './App';
 
+const customTheme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        color: 'white',
+        bg: "#202020",
+      },
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <Router>
         <Routes>
           <Route path="/*" element={<App />} />
